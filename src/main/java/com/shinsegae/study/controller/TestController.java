536b2a41ca0170e0224f3ab21fded1094fd9a8a5
김윤소 <>
2022-04-05 15:47:19 +0900

    최초커밋

---
+++ src/main/java/com/shinsegae/study/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.shinsegae.study.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String testGet(){
+        return "Success";
+    }
+}
